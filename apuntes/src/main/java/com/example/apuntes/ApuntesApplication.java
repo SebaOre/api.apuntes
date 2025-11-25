4e75db258b11184932ef3bbe317a1f4a67a6446b
Sebastian Orellana <seb.orellanab@duocuc.cl>
2025-11-25 20:35:58 -0300

    Add files via upload

---
+++ apuntes/src/main/java/com/example/apuntes/ApuntesApplication.java
@@ -0,0 +1,13 @@
+package com.example.apuntes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuntesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuntesApplication.class, args);
+	}
+
+}
